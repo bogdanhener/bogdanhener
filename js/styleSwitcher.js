9416d557f9f2437138a9b7487c91cd439af773f5
Bogdan Hener <bogdan@heners94.com>
2020-07-15 15:39:35 +0300

    initial commit

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,29 +1,41 @@
 
-const links = document.querySelectorAll(".alternate-style");
+   const links=document.querySelectorAll(".alternate-style"),
+         totalLinks=links.length;
 
-function setActiveStyle (color){
-    for(let i = 0; i < links.length; i++){
-        if(color === links[i].getAttribute('title')){
-            links[i].removeAttribute('disabled');
-        } else {
-            links[i].setAttribute('disabled', 'true');
-        }
-    }
-}
 
-// Body Skin
+ function setActiveStyle(color) {
+      for(let i=0; i<totalLinks; i++){
+      	if(color === links[i].getAttribute("title")){
+      		links[i].removeAttribute("disabled");
+      	}
+      	else{
+      	  links[i].setAttribute("disabled","true");	
+      	}
+      }
+ 	 
+ }
 
-const bodySkin= document.querySelectorAll('.body-skin');
-for(let i = 0; i< bodySkin.length; i++){
-    bodySkin[i].addEventListener('change', function(){
-        if(this.value === "dark"){
-            document.body.className= "dark";
-        } else {
-            document.body.className='';
-        }
-    })
-}
+  //  body Skin
 
-document.querySelector('.toggle-style-switcher').addEventListener('click', () => {
-    document.querySelector('.style-switcher').classList.toggle('open');
-})
+    const bodySkin=document.querySelectorAll(".body-skin"),
+          totalBodySkin=bodySkin.length;
+      for(let i=0; i<totalBodySkin; i++){
+      	bodySkin[i].addEventListener("change",function(){
+      	   if(this.value === "dark"){
+      	   	document.body.className="dark";
+      	   }
+      	   else{
+      	   	 document.body.className="";
+      	   }
+      	})
+      }
+
+
+
+ document.querySelector(".toggle-style-switcher").addEventListener("click",() =>{
+ 	document.querySelector(".style-switcher").classList.toggle("open");
+ })
+
+
+
+
